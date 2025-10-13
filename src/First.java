0a8287886d6d45b43923f74325d4b68aa5546d57
Harsh Parekh <aparekh.harsh@gmail.com>
2025-10-13 17:59:03 +0530

    First Commit

---
+++ src/First.java
@@ -0,0 +1,9 @@
+public class First {
+    public static void main(String[] str) {
+        System.out.println("Hello, World!");
+//        System.out.println(args[0]); //written dsfsjdf on command line got this as reponse
+        //we can name anything to the String array
+        System.out.println(str[0]); //written dsfsjdf on command line got this as reponse
+        System.out.println(str[1]); //written edterfdsf on command line got this as reponse
+    }
+}
---
+++ src/Second.java
@@ -0,0 +1,83 @@
+public class Second {
+    public static void main(String[] args) {
+        //various Datatypes
+        //whole numbers
+//        int age = 21;
+//        byte age1= - 21;
+//        short age2 = 21;
+//        long age3 = 2134354354353453L;
+//
+//        System.out.println(Byte.MIN_VALUE); //-128
+//        System.out.println(Byte.MAX_VALUE); //127
+//        System.out.println(Short.MIN_VALUE); //-32768
+//        System.out.println(Short.MAX_VALUE); //32767
+//        System.out.println(Integer.MIN_VALUE); //-2147483648
+//        System.out.println(Integer.MAX_VALUE); // 2147483647
+//        System.out.println(Long.MIN_VALUE); //-9223372036854775808
+//        System.out.println(Long.MAX_VALUE); //9223372036854775807
+//
+//
+//        //Decimal numbers
+//        float age4 = 21.534354534545f; //21.534355
+//        double age5 = 2.34354535454345; //2.34354535454345
+//        System.out.println(age4);
+//        System.out.println(age5);
+//
+//        System.out.println(Float.MIN_VALUE); //1.4E-45
+//        System.out.println(Float.MAX_VALUE); //3.4028235E38
+//        System.out.println(Double.MIN_VALUE); //4.9E-324
+//        System.out.println(Double.MAX_VALUE); //1.7976931348623157E308
+
+
+        //Characters
+//            char myChar = 'a';
+//            int a = 1;
+//            int Char2 = myChar;
+//        System.out.println(Char2); //returns 97 ascii value of a
+//        System.out.println(myChar + a); //returns 98
+//        int three = '3';
+//        System.out.println(three); //returns 51
+//        System.out.println('3' + 3); //returns 54
+//        System.out.println('3' - 3); //returns 48
+//            String name = "Harsh";
+//        System.out.println(name + 3); //Harsh3
+//        System.out.println(name + '3'); //Harsh3
+//        System.out.println(name + three); //Harsh51
+//        System.out.println(name + myChar); //Harsha
+
+//        System.out.println((int) (Character.MIN_VALUE)); // returns empty -> 0 (integer value)
+//        System.out.println((int) (Character.MAX_VALUE)); //returns ? -> 65535 (integer value)
+
+//        char symbol = 10084;
+//          char symbol = '\u2764';
+//        System.out.println(symbol); //prints heart as a symbol
+
+        //Boolean
+//        boolean isEligible = false;
+//        System.out.println(isEligible);
+
+        //widening and narrowing
+
+//        byte e = 10; //1 byte
+//        short g = e; //2 bytes
+//        int a = 10; //4 bytes
+//        long b = a; //8 bytes
+//        float c = a; //4 bytes
+//        double d = a ;//8 bytes
+
+//        float f = 1.7f;
+//        int g = f;    //Required type:int
+                        // Provided:float
+//        int g = (int) f;
+
+
+        //variable naming convention
+
+        //case-sensitive;
+        //can be Letters, Digits, $ sign or Underscore(_);
+        //Must Begin with Letter, Dollar Sign ($), or Underscore(_);
+        //cannot use Java keywords as variable names;
+        //camel case;
+
+    }
+}
