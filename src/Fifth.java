a74b4a15fd2913b59526bb9e789075d73f79faa2
Harsh Parekh <aparekh.harsh@gmail.com>
2025-10-17 11:12:00 +0530

    Learned about loops

---
+++ src/Fifth.java
@@ -0,0 +1,105 @@
+public class Fifth {
+    public static void main(String[] args) {
+        //loops
+        //while loop
+//        int i = 0;
+//        while(i<10){
+//            System.out.println("Hello World!" + " "+ i +"th"  + " time");
+//            i++;
+//        }
+
+        //do while loop
+//        int i = 0;
+//        do {
+//            System.out.print(i + " ");
+//            i++;
+//        }while(i<50);
+
+        //for loop
+//        for(int i = 0; i<=10 ;i++){
+//            System.out.print(i + " ");
+//        }
+
+        /*
+        * 1
+        * 10
+        * 100
+        * 1000
+        * 10000
+        * 100000
+        * */
+
+//        for(int i=1, j = 1; j<=6; i=i*10,j++ ){
+//            System.out.println(i);
+//        }
+
+//        for(int i =1;i<=10;i++){
+//            System.out.println(7 * i);
+//        }
+
+        //sum of n natural nos
+
+//        int sum=0;
+//        int i = 0;
+//        while(i<=10){
+//            sum+=i;
+//            i++;
+//        }
+//        System.out.println(sum);
+        // count number of digts
+
+        // 34323467
+
+//        int count = 0;
+//
+//        int n = 34323467;
+//        while(n > 0){
+//            n = n / 10;
+//            count++;
+//        }
+//        System.out.println(count);
+
+        //factorial for a number
+        // 6! = 720
+//
+//        int n = 6;
+//        int fact = 1;
+//       while (n>0){
+//           fact = fact * n;
+//           n--;
+//       }
+//        System.out.println(fact);
+
+        /*
+        *   *
+        *   **
+        *   ***
+        *   ****
+        *   *****
+        */
+
+//        for(int i = 0;i<=5;i++){
+//            for(int j = 1; j <= i; j++){
+//                System.out.print("*");
+//            }
+//            System.out.println();
+//        }
+
+        // break and continue keyword
+
+
+
+
+
+
+
+
+
+
+
+        //for-each loop
+
+
+
+    }
+}
